bcd4744c43717cf60044cba3530d20259c9e6b8e
EduMendietaSTEM <145747244+EduMendietaSTEM@users.noreply.github.com>
2023-10-09 13:57:33 +0200

    JugandoconBucles

---
+++ JugandoConBucles.py
@@ -0,0 +1,22 @@
+"""
+Contexto: Estas organizando una fiesta de halloween y quieres
+calcular cuantos dulces necesitas comprar para los niños que 
+vendrán a tu casa.
+
+Caso 1: Tenemos 500 dulce y vamos a repartirlos.
+"""
+
+
+
+import random
+
+#Funciones:
+
+
+#Codigo principal: 
+numMaxDulcesNinioPorNinios = 5
+numDulces = 500
+esDulce = True
+
+while (esDulce):
+    print("ÑamÑam")
